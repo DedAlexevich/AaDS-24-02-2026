87425213bb9b0e6a521cee86a2497e33a34f2bb2
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-02-24 08:32:18 +0300

    add begin and hasNext

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+template< class T >
+struct Vec {
+  T* data;
+  size_t s;
+};
+template< class T >
+struct VecIt {
+  T* data;
+  size_t i, s;
+};
+
+template< class T >
+VecIt< T > begin(Vec<T> d)
+{
+  return {d.data, 0, d.s};
+}
+
+template< class T >
+bool hasNext(VecIt<T> it)
+{
+  return it.i == it.s;
+}
+
+
+
 int main()
 {
   std::cout << "Hello, World!\n";
